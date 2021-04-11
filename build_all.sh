7f5653885e85a41607086e51a88aaac93024363a
Emily Ingalls <emily@ingalls.rocks>
2021-04-11 02:23:19 -0700

    update build scripts a bit

--- build_all.sh
+++ build_all.sh
@@ -7,14 +7,17 @@ rm -rf build bin
 mkdir -p build/486quake
 mkdir -p bin
 
+oasm=yes
+olto=yes
+
 for cpu in 386 486 586 686; do
-    for olevel in Os O2 O3; do
-        for olto in yes no; do 
-            for oasm in yes no; do
+    for olevel in Os O3; do
+        #for olto in yes no; do 
+            #for oasm in yes no; do
                 make -f Makefile.dos clean
                 make -j8 -f Makefile.dos OCPU=$cpu OLEVEL=$olevel OLTO=$olto OASM=$oasm
                 cp build/*.exe bin/
-            done
-        done
+            #done
+        #done
     done
 done
