8c32b289ad34f7e719fcac5d5e17a37c177f61f6
linear cannon <dev@linear.network>
2022-01-23 04:46:52 -0800

    fix macro definition in asm_i386.h

--- src/asm_i386.h
+++ src/asm_i386.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define __ASM_I386__
 
 #ifdef NO_ASM_PREFIX
-#define C(label) ##label
+#define C(label) label
 #else
 #define C(label) _##label
 #endif
